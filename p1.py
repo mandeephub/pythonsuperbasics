6327bced6b822e009b16ede2764ef06c3e6b3fa5
mandeephub <suggestios.live@gmail.com>
2023-02-26 23:15:13 +0530

    p1 added

---
+++ p1.py
@@ -0,0 +1,11 @@
+#print the following string
+"""
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+"""
+
+print("Twinkle, twinkle, little star,\n\t\t How I wonder what you are!\n\t\t\t Up above the world so high,\n\t\t\t Like a diamond in the sky. \nTwinkle, twinkle, little star, \n\t\tHow I wonder what you are")
